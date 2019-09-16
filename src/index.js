b3eb7ccc9be152655d698f134253df46320a0e9b
liuqing <qing_liu@bestsign.cn>
2019-09-16 14:19:03 +0800

    feat: context test

--- src/index.js
+++ src/index.js
@@ -4,10 +4,86 @@ import './index.css';
 import App from './App';
 import CommentApp from './CommentApp';
 import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
+
+class Index extends Component {
+    static childContextTypes = {
+        themeColor: PropTypes.string
+    }
+
+    constructor() {
+        super();
+        this.state = { themeColor: 'green'};
+    }
+
+    getChildContext() {
+        return {
+            themeColor: this.state.themeColor
+        };
+    }
+
+    render () {
+      return (
+        <div>
+          <Header />
+          <Main />
+        </div>
+      )
+    }
+  }
+  
+  class Header extends Component {
+    render () {
+      return (
+      <div>
+        <h2>This is header</h2>
+        <Title />
+      </div>
+      )
+    }
+  }
+  
+  class Main extends Component {
+    render () {
+      return (
+      <div>
+        <h2>This is main</h2>
+        <Content />
+      </div>
+      )
+    }
+  }
+  
+  class Title extends Component {
+      static contextTypes = {
+          themeColor: PropTypes.string
+      }
+    render () {
+      return (
+        <h1
+            style={
+                { color: this.context.themeColor }
+            }>
+            React.js 小书标题
+        </h1>
+      )
+    }
+  }
+  
+  class Content extends Component {
+    render () {
+      return (
+      <div>
+        <h2>React.js 小书内容</h2>
+      </div>
+      )
+    }
+  }
 
 ReactDOM.render(
-    <CommentApp>
-    </CommentApp>,
+    // <CommentApp>
+    // </CommentApp>,
+    <Index/>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
